149eb6e666016a5eef55de0c60f1a2cc4a63d01d
taoufik <farouki.taoufik@gmail.com>
2018-12-20 01:25:51 +0000

    add login and logout functionality

--- index.php
+++ index.php
@@ -5,4 +5,18 @@ if (Session::exists("success")) {
   echo Session::flash("success");
 }
 
+
+$user = new User();
+if($user->isLoggedIn()){
+?>
+  <p> Hello <a href="#"><?php echo escape($user->data()->username); ?></a>!</p>
+  <ul>
+    <li> <a href="logout.php">Log Out</a></li>
+  </ul>
+<?php
+}else{
+?>
+  <p>You need to <a href="login.php">login</a> or <a href="register.php">register</a>!</p>
+<?php
+}
 ?>
